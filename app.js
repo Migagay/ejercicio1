4af460ca5d4800f4d8ece57d94f1ca9e19af7386
Migagay <millaray.gomez02@inacapmail.cl>
2024-05-29 00:23:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let prestamos = JSON.parse(localStorage.getItem("prestamos")) || { Hipotecario: [], Automotriz: [], Consumo: [] };
+
+function datos(formulario) {
+    
+    const { nombre, tipo_prestamo } = formulario;
+
+    prestamos[tipo_prestamo.value].push(nombre.value);
+
+    localStorage.setItem("prestamos", JSON.stringify(prestamos));
+
+    // Limpiar el formulario
+    formulario.reset();
+}
+
+function resetearRepositorio() {
+    // Eliminar los datos del almacenamiento local
+    localStorage.removeItem("prestamos");
+
+    // Restablecer la variable productos a su estado inicial
+    prestamos = {  Hipotecario: [], Automotriz: [], Consumo: [] };
+
+    console.log("Repositorio reseteado");
+}
+
+function reporte() {
+    let mensaje = "Hipotecarios: " + prestamos.Hipotecario.length + "\n" +
+                  "Automotriz: " + prestamos.Automotriz.length + "\n" +
+                  "Consumo: " + prestamos.Consumo.length + "\n\n" ;
+
+    alert(mensaje);
+}
+// con la siguiente parte guardamos para ser llamado en la pagina resumen
+document.getElementById("hipotecarios").textContent = prestamos.Hipotecario.length;
+document.getElementById("automotriz").textContent = prestamos.Automotriz.length;
+document.getElementById("consumo").textContent = prestamos.Consumo.length;
